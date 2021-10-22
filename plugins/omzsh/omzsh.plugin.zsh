cd3c9480feb5a6891e637c8e04c206460594a44f
Bryan Keeney <74679971+keeneydev@users.noreply.github.com>
2021-10-21 17:56:09 -0700

    Add oh my zsh shortcuts

---
+++ plugins/omzsh/omzsh.plugin.zsh
@@ -0,0 +1,8 @@
+# Open editor for your .zshrc
+alias zshconfig="code ~./zshrc"
+# Reload all plugins to pickup new changes in current terminal
+alias zshreload="source $HOME/.zshrc"
+# Change to config directory
+alias cdzshconfig='cd ~/.oh-my-zsh/custom'
+# Open editor for omzsh.plugin.zsh
+alias omzshconfig='code ~/.oh-my-zsh/custom/plugins/omzsh.plugin.zsh'
